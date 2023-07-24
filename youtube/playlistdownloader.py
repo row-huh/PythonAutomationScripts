bd0b1e6af6118926024c7bb1d973f35575f34812
Roha Pathan <rohaabropathan@gmail.com>
2023-07-24 19:11:03 +0500

    added code to download a playlist

--- youtube/playlistdownloader.py
+++ youtube/playlistdownloader.py
@@ -1,1 +1,10 @@
 from pytube import Playlist
+
+p = Playlist("https://youtube.com/playlist?list=PLrolabKNtdOIrGLzdwcVVQkm1XgA-dZ4m")
+
+print(f'Downloading: {p.title}')
+
+
+for video in p.videos:
+    video.bypass_age_gate()
+    video.streams().first().download()
