5f2b0903cc55709b8d30fa49cc24d90ecedfaaa3
Roha Pathan <rohaabropathan@gmail.com>
2023-07-25 11:20:35 +0500

    created getPlaylistlink()

--- youtube/playlistdownloader.py
+++ youtube/playlistdownloader.py
@@ -4,7 +4,7 @@ import re
 from moviepy.editor import AudioFileClip
 
 def main():
-    playlist_link = input("Enter playlist link: ")
+    playlist_link = getPlaylistlink()
     p = Playlist(playlist_link)
 
 
@@ -12,24 +12,29 @@ def main():
 
 
     for video in p.videos:
-        try:
-            video.bypass_age_gate()
-            video.streams.first().download('C:/Users/rohaa/Downloads')
-            print("Downloaded Succesfully")
-        except exceptions.AgeRestrictedError:
-            print(f"Age restricted error on {video.title}, moving on")
-        except:
-            print("Error occurred way down bruh")
+        ...
         
 
 # checks if the user inputted string is a valid playlist string or not        
-def getPlaylist():
+def getPlaylistlink():
     link = input("Enter Playlist link: ")
     regex = r"^(https://)?(www\.)?(youtube\.com|youtu\.be)/playlist\?list=([a-zA-Z0-0]+)"
     match = re.match(regex, link)
     while not match:
         link = input("Invalid link, Enter again: ")
         match = re.match(regex, link)
+    return link
+
+
+def download_video(video):
+    try:
+            video.bypass_age_gate()
+            video.streams.first().download('C:/Users/rohaa/Downloads')
+            print("Downloaded Succesfully")
+    except exceptions.AgeRestrictedError:
+        print(f"Age restricted error on {video.title}, moving on")
+    except:
+        print("Error occurred way down bruh")
 
 
 
@@ -37,6 +42,10 @@ def getPlaylist():
 # convert to mp3
 def convertToMp3(video):
     ...
+    
+    
+    
+    
     
 if __name__ == "__main__":
     main()
