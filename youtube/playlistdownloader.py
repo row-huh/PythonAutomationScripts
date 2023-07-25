fafcd47c51d23e2dcd82170b6167b177bdd3b891
Roha Pathan <rohaabropathan@gmail.com>
2023-07-25 15:29:10 +0500

    added choice for mp3 and mp4

--- youtube/playlistdownloader.py
+++ youtube/playlistdownloader.py
@@ -6,12 +6,11 @@ import subprocess
 # main function
 def main():
     # ask the user if they want to download video or a playlist
-    choice = input("Enter the number adjacent to: \n[1] Download a YouTube video\n[2] Download a YouTube playlist\nEnter: ")
+    choice = input("Enter the number adjacent to: \n[1] MP3\n[2] MP4\nEnter: ")
     while choice != "1" and choice != "2":
         choice = input("\n\nInvalid choice!!\n[1] Download a YouTube video\n[2] Download a YouTube playlist\nEnter: ")
-        
     if choice == "1":
-        ...
+        
     elif choice == "2":
         ...        
  
@@ -21,8 +20,9 @@ def main():
 # checks if the user inputted string is a valid playlist string or not        
 def getPlaylistlink():
     link = input("Enter Playlist link: ")
-    regex = r"^(https://)?(www\.)?(youtube\.com|youtu\.be)/playlist\?list=([a-zA-Z0-0]+)"
-    match = re.match(regex, link)
+    regex1 = r"^(https://)?(www\.)?(youtube\.com|youtu\.be)/playlist\?list=([a-zA-Z0-0]+)"
+    regex2 = r"^(https?://)?(www\.)?(youtube\.com|youtu\.be)/(watch\?v=)?([a-zA-Z0-9_-]+)"
+    match = re.match(regex1, link)
     while not match:
         link = input("Invalid link, Enter again: ")
         match = re.match(regex, link)
@@ -32,7 +32,7 @@ def getPlaylistlink():
 
 def getVideolink():
     link = input("Enter Video link: ")
-    regex = r"^(https?://)?(www\.)?(youtube\.com|youtu\.be)/(watch\?v=)?([a-zA-Z0-9_-]+)"
+    regex = 
     match = re.match(regex, link)
     while not match:
         link = input("Invalid link, Enter again: ")
