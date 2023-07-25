325d9982874cd1ebd7b758b87de0b7346dae7a84
Roha Pathan <rohaabropathan@gmail.com>
2023-07-25 15:30:34 +0500

    made getPlaylistlink and getVideolink one function

--- youtube/playlistdownloader.py
+++ youtube/playlistdownloader.py
@@ -10,35 +10,20 @@ def main():
     while choice != "1" and choice != "2":
         choice = input("\n\nInvalid choice!!\n[1] Download a YouTube video\n[2] Download a YouTube playlist\nEnter: ")
     if choice == "1":
-        
+        ...
     elif choice == "2":
         ...        
  
 
-
-
 # checks if the user inputted string is a valid playlist string or not        
-def getPlaylistlink():
+def getLink():
     link = input("Enter Playlist link: ")
-    regex1 = r"^(https://)?(www\.)?(youtube\.com|youtu\.be)/playlist\?list=([a-zA-Z0-0]+)"
-    regex2 = r"^(https?://)?(www\.)?(youtube\.com|youtu\.be)/(watch\?v=)?([a-zA-Z0-9_-]+)"
-    match = re.match(regex1, link)
-    while not match:
-        link = input("Invalid link, Enter again: ")
-        match = re.match(regex, link)
-    return link
-
-
-
-def getVideolink():
-    link = input("Enter Video link: ")
-    regex = 
+    regex = r'(https?://)?(www\.)?(youtube\.com|youtu\.be)/(watch\?v=|playlist\?list=)([a-zA-Z0-9_-]+)'
     match = re.match(regex, link)
     while not match:
         link = input("Invalid link, Enter again: ")
         match = re.match(regex, link)
     return link
-    
     
 
 def downloadMP3(link):
